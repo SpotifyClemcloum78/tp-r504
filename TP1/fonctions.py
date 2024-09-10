f0a333127c0ad95728d92757bf697b7e5b2e2b9f
SpotifyClemcloum78 <clementsavoye78@gmail.com>
2024-09-10 11:28:49 +0200

    Changement 2

---
+++ TP1/fonctions.py
@@ -0,0 +1,8 @@
+def puissance(a, b):
+
+    return a ** b
+a = float(input("Entrez a : "))
+b = float(input("Entrez b : "))
+
+resultat = puissance(a, b)
+print(f"{a} à la puissance {b} est {resultat}")
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+import fonctions as f
+while True:
+    try:
+   
+        b = float(input("Entrez un nombre 1 (ou appuyez sur CTRL-C pour quitter) : "))
+        a = float(input("Entrez un nombre 2 (ou appuyez sur CTRL-C pour quitter) : "))
+
+     
+        carre1 = a ** 2
+        
+        
+        res = f.puissance(a,b)
+        
+        print(res)
+    except KeyboardInterrupt:
+
+        print("\nInterruption reçue. Fin du programme.")
+        break 
