1aa5ddc74e12bd49bcaa62dca4c747865aef4db4
SpotifyClemcloum78 <clementsavoye78@gmail.com>
2024-09-12 11:27:52 +0200

    nouveau

--- TP1/test.py
+++ TP1/test.py
@@ -8,3 +8,12 @@ def test_1 ( ) :
 def test_2 ( ) :
         assert f.puissance (-1, 3) == -1
         assert f.puissance (2, -2) == 0.25
+
+def test_3 ( ) :
+        assert f.puissance (2, 0) == 1
+
+def test_4 ( ) :
+        assert f.puissance (0, 1) == 0
+
+def test_5 ( ) :
+        assert f.puissance (0, -1) == 
