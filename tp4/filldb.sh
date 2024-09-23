94981af61d7a52436bcc7b101f68adeddf308a3e
SpotifyClemcloum78 <clementsavoye78@gmail.com>
2024-09-23 14:31:35 +0200

    auppression

--- tp4/filldb.sh
+++
@@ -1,1 +0,0 @@
-mysql -u root -p'foo' -h 127.0.0.1 --port=3307 < data.sql
--- tp4/purge.sh
+++
@@ -1,5 +0,0 @@
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker network prune -f
-docker volume prune -f
-docker system prune -f
