fe160c99801be6fbb74d321fa94d6f69fa06c492
SpotifyClemcloum78 <clementsavoye78@gmail.com>
2024-09-23 13:52:19 +0200

    New10

--- tp4/build_image.sh
+++ tp4/build_image.sh
@@ -3,10 +3,6 @@ DOCKERFILE_PATH="Dockerfile1"
 
 echo "Construction de l'image Docker avec le nom: $IMAGE_NAME"
 docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH .
+exit 0
 
-DEBIAN_SIZE=$(docker images debian:11 --format "{{.Size}}")
-TP4_SIZE=$(docker images $IMAGE_NAME --format "{{.Size}}")
 
-# Afficher la taille des images
-echo "Taille de l'image Debian 11: $DEBIAN_SIZE"
-echo "Taille de l'image $IMAGE_NAME: $TP4_SIZE"
