ca3ba038889c4327b43342bb7e8be0fa1cc4d1c4
SpotifyClemcloum78 <clementsavoye78@gmail.com>
2024-10-01 15:55:36 +0200

    TP-LB

---
+++ TP-LB/tp-C/appli/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    container_id = os.environ.get('HOSTNAME')
+    return f"Hello from container {container_id}"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
